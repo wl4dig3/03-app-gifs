8fddbedcc95da44b6571c6e393cd80e783dcb53b
Wladimir Guevara Berbesi <wladige@gmail.com>
2025-04-07 14:24:38 -0400

    feat: creadas rutas hijas de nuevos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+        children: [
+            {
+                path: 'search',
+                loadComponent: () => import('./gifs/pages/search-page/search-page.component'),
+            },
+            {
+                path: 'trending',
+                loadComponent: () => import('./gifs/pages/trending-page/trending-page.component'),
+            },
+        ],
+    },
+    
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    },
+];
